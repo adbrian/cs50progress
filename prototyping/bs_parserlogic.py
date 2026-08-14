b4a7b5af8560397b6545058078573d3fffd1c164
Ian <113297674+adbrian@users.noreply.github.com>
2026-08-14 13:04:26 +0530

    Parsed data written to a list of dicts
    Previously limited to parsing and printing only

--- prototyping/bs_parserlogic.py
+++ prototyping/bs_parserlogic.py
@@ -1,12 +1,19 @@
+import json
 import re
 
 from bs4 import BeautifulSoup
+
+pset_list = []
 
 with open("dumps.html") as file:
     soup = BeautifulSoup(file, "lxml")
     for item in soup.find_all("li"):
         tag = item.a.text.strip()
-        pset = re.sub(r".*/", "", tag)
-        print(pset)
+        pset_name = re.sub(r".*/", "", tag)
+        # print(pset_name)
         pset_date = item.find(attrs={"data-moment": "LLLL"}).text.strip()
-        print(pset_date)
+        # print(pset_date)
+        pset_dict = {"pset_name": pset_name, "pset_date": pset_date}
+        pset_list.append(pset_dict)
+
+print(json.dumps(pset_list, indent=2))
